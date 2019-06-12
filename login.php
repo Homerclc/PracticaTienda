a97286b2c7f1c175f37254c1367df857d8d2d87a
Homerclc <homerclc92@gmail.com>
2019-06-13 00:11:48 +0200

    Entrega_final

--- login.php
+++ login.php
@@ -1,1 +1,25 @@
+<?php
+session_start();
+include 'DB.php';
+require('Smarty.class.php');
+$plantilla = new Smarty;
+$plantilla->template_dir = 'vistas/templates/template';
+$plantilla->compile_dir = 'vistas/templates/template_c';
+$error=$_GET['error'];
+//asignamos los valores para personalizar plantilla, para sustituir las variables de la misma
+if (isset($_POST['enviar'])) {
+    $nombre = $_POST['usuario'];
+    $pass = $_POST['password'];
+    if (db::verificaCliente($nombre, $pass)) {
+        //si es correcto guardamos el nombre en una variable de sesion
+        $_SESSION['nombre']=$nombre;
+        header('Location: http://localhost/PracticaTienda/productos.php');
+    } else {
+        $plantilla->assign('error', 'Introduce nombre y contraseña de nuevo');
+    }
+}
+
+$plantilla->assign('error', $error);
+$plantilla->display('login.tpl');
+?>
 
