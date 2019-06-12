e14937a5c3db01582258d23dc1adca1bd1af8aba
Homerclc <homerclc92@gmail.com>
2019-06-13 00:10:27 +0200

    Entrega_final

--- Cesta.php
+++ Cesta.php
@@ -1,1 +1,61 @@
+<?php
+class Cesta {
+
+   private $productos = [];
+
+
+    public function __construct()
+    {
+    }
+
+
+    public function add_producto($cod)
+    {
+        if (key_exists($cod, $this->productos)) {
+            $this->productos[$cod]['unidades'] ++;
+        } else {
+            $this->productos[$cod]['unidades'] = 1;
+            $this->productos[$cod]['productos'] = DB::obtieneProducto($cod);
+        }
+     
+    }
+    
+    public function descuentaProducto($cod){
+        if($this->productos[$cod]['unidades']==1){
+            unset($this->productos[$cod]);
+        }else{
+            $this->productos[$cod]['unidades']--;
+        }
+    }
+    
+    public function getProductos(){
+        return $this->productos;
+    }
+
+    public static function obtener_cesta()
+    {
+        //leo variable de sesion
+        if (isset($_SESSION['cesta'])) {
+            $cesta = unserialize($_SESSION['cesta']);
+        } else {
+            $cesta = new Cesta();
+        }
+
+        return $cesta;
+    }
+
+    public function guardar_cesta()
+    {
+        //leo variable de sesion
+        $_SESSION['cesta'] = serialize($this);
+
+    }
+    
+    public function vaciarCesta(){
+        $this->productos=[];
+        
+    }
+
+
+}
 
